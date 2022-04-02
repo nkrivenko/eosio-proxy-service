ee02f572534869b64dfc2f7e52b7a09889faa7af
Nikolai Krivenko <nkrivenko35@gmail.com>
2022-04-02 20:24:46 +0300

    [#1] Create the backbone of project

---
+++ src/main/java/com/gtlab/godjigame/wax/eosioproxyservice/EosioProxyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gtlab.godjigame.wax.eosioproxyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EosioProxyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EosioProxyServiceApplication.class, args);
+    }
+
+}
